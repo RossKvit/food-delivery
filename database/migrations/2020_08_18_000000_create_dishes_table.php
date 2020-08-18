b521a7b67d4b5bc34c199f60bbc80500460ea44b
RossKvit <rostikkvit@gmail.com>
2020-08-18 21:31:14 +0300

    add restourants, dishes migrations

---
+++ database/migrations/2020_08_18_000000_create_dishes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dishes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('description'); //->unique();
+            $table->integer('restaurant_id');
+            $table->integer('price');
+            $table->integer('weight');
+            $table->integer('type_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dishes');
+    }
+}
